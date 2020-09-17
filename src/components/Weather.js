6f6642dcbcad37d0bb20b8a57c7ba1b22d8d7fec
tement <teemu.ylanne@opp.eduvantaa.fi>
2020-09-17 11:12:17 +0300

    Changes to Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from "react";
 import { VictoryChart, VictoryTooltip, VictoryGroup, VictoryLine, VictoryScatter, VictoryAxis, VictoryVoronoiContainer } from "victory";
 
 function Weather() {
+  const today = new Date();
+  const date = today.getDate() + "." + parseInt(today.getMonth() + 1) + "." + today.getFullYear();
+
   const initWeather = [];
   const [weather, setWeather] = useState(initWeather);
 
-  const tempData = [
+  useEffect(() => {
+    fetch("https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==&amount=50")
+    .then(response => response.json())
+    .then(json => setWeather([...json]));
+  }, []);
+
+  const tempData_example = [
     // x = date, y = temperature
     {x: "1.1.", y: -5},
     {x: "2.1.", y: -6},
@@ -15,7 +24,7 @@ function Weather() {
     {x: "6.1.", y: 1}
   ]
 
-  const humData = [
+  const humData_example = [
     // x = date, y = percentage
     {x: "1.1.", y: 50},
     {x: "2.1.", y: 60},
@@ -25,17 +34,24 @@ function Weather() {
     {x: "6.1.", y: 65}
   ]
 
-  /*
-  useEffect(() => {
-    fetch("https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=60.3049&lon=25.0207")
-    .then(response => response.json())
-    .then(json => setWeather([...json]));
-  }, []);
-  */
+  let tempData = [];
+  let humData = [];
+
+  const rows = () => weather.slice(0,24).reverse().map(temphum => {
+    const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0];
+    const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ':' + temphum.PublishedAt.split('T')[1].split(':')[1];
+    tempData.push({x: String(measurementTime), y: parseInt(temphum.Temp)});
+    humData.push({x: String(measurementTime), y: parseInt(temphum.Hum)});
+    return(
+      <div>
+        <b>Pvm: </b>{measurementDate}, <b>klo: </b>{measurementTime} ------ <b>Ilmankosteus: </b>{temphum.Hum.split('.')[0]} % ------ <b>Lämpötila: </b>{temphum.Temp.split('.')[0]} °C
+      </div>
+    )
+  })
 
   return(
-    <div>
-      <p><b>Lämpötila (°C)</b></p>
+    <div align="center">
+      <h3>Lämpötila (°C)</h3>
       <VictoryChart
         domainPadding={{x: 30, y:10}}
         width={1000}
@@ -83,6 +99,16 @@ function Weather() {
           />
         </VictoryGroup>
       </VictoryChart>
+      <div>
+        <h3>Piirrettävän kaavion data</h3>
+      </div>
+      <div>
+        <b>Tänään on: {date}</b>
+      </div>
+      <div>
+        {rows()}
+      </div>
+      <br />
     </div>
   )
 };
